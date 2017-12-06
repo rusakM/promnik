4d02b9f86b34f421db6a166883de8ebf364fc253
rusakM <rusakkk@live.com>
2017-12-06 21:04:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>UKS - Promnik Gończyce</title>
+    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/45b87b5955.css">
+    <link rel="stylesheet" href="css/styl.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <header>
+      <nav>
+           <i id="menu" class="fa fa-bars" aria-hidden="true"></i>
+           <ul id="list">
+              <img src="img/page/logo.png" alt="logo" id="logo">
+               <li>Aktualności</li>
+               <li>Terminarz</li>
+               <li>Tabela</li>
+               <li>O klubie</li>
+               <li>Kadry</li>
+               <li>Galeria</li>
+               <li>Sponsorzy</li>
+               <li>Kontakt</li>
+          </ul>
+        </nav>
+        <img src="img/page/background.jpg" id="background">
+        <h1 id="slogan"><span>PROMNIK</span> GOŃCZYCE</h1>
+        <!--<div id="arrow"><i class="fa fa-chevron-down" aria-hidden="true"></i></div>-->
+    </header>
+    <section id="news">
+    </section>
+</body>
+</html>
