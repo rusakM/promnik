1eee4183d5a801ef93576db41615adaa39ae2f37
rusakM <rusakkk@live.com>
2017-12-08 15:09:31 +0100

    edycja zawodnikow v2

--- admin/panel/model/player_edit.php
+++ admin/panel/model/player_edit.php
@@ -3,10 +3,22 @@
     require_once('../admin/scripts/connect.php');
     $con = mysqli_connect($host, $usr, $pass, $db);
     $id = $_GET['id'];
-    $test = mysqli_fetch_array(mysqli_query($con, "SELECT  FROM players WHERE id='$id'"));
+    $test = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM players WHERE player_id='$id'"));
+    $cols = mysqli_query($con, "SELECT * FROM players");
+    $columns = [];
+    while($col = mysqli_fetch_field($cols)) {
+        array_push($columns, $col->name);
+    }
     $data = [];
     foreach($_POST as $row) {
         array_push($data, $row);
+    }
+    $b = 0;
+    $c = 2;
+    for($a = 2; $a < count($test) - 1; $a++) {
+        if($test[$a] !== $data[$b]) {
+            mysqli_query($con, "UPDATE players SET ".$columns[$c]."=".$data[$b]."WHERE player_id='$id'");
+        }
     }
 
     
